c4e9f87027d35346e2bc2513b27eb90a3604cd3b
Divyang Mistry <divyangmistry7777@gmail.com>
2019-06-22 18:08:15 +0530

    created basic structure of app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventCategoriesComponent } from './event-categories/event-categories.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventFormComponent } from './event-form/event-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventCategoriesComponent,
+    EventListComponent,
+    EventFormComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
