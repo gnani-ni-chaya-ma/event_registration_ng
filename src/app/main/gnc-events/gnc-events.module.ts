d116bd1b5cc479db9b9fedda000ed6f56b14f155
rajpar29 <rajuparlia123321@gmail.com>
2019-06-29 22:10:06 +0530

    Completed registration form

--- src/app/main/gnc-events/gnc-events.module.ts
+++ src/app/main/gnc-events/gnc-events.module.ts
@@ -4,6 +4,10 @@ import { RouterModule } from '@angular/router';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { ListComponent } from './list/list.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatMenuModule, MatRippleModule, MatTableModule, MatToolbarModule, MatAutocompleteModule, MatRadioModule } from '@angular/material';
+import { FuseSharedModule } from '@fuse/shared.module';
+import { FuseConfirmDialogModule, FuseSidebarModule } from '@fuse/components';
 
 const routes = [
   {
@@ -30,7 +34,24 @@ const routes = [
   declarations: [RegistrationFormComponent, ListComponent, CategoriesComponent],
   imports: [
     RouterModule.forChild(routes),
+    FormsModule,
 
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatMenuModule,
+    MatRippleModule,
+    MatTableModule,
+    MatToolbarModule,
+    MatAutocompleteModule,
+    MatRadioModule,
+
+    FuseSharedModule,
+    FuseConfirmDialogModule,
+    FuseSidebarModule,
     CommonModule
   ]
 })
