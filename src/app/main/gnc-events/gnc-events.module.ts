2865e023f247bc0a41796b3090f0ef9d8ab0da36
DenishChovatiya <denishchovatiya8@gmail.com>
2019-06-29 22:19:37 +0530

    completed categories page

--- src/app/main/gnc-events/gnc-events.module.ts
+++ src/app/main/gnc-events/gnc-events.module.ts
@@ -4,6 +4,11 @@ import { RouterModule } from '@angular/router';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { ListComponent } from './list/list.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { MatIconModule, MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatDialogModule, MatChipsModule, MatFormFieldModule, MatInputModule, MatListModule, MatMenuModule, MatProgressBarModule, MatRippleModule, MatSidenavModule, MatToolbarModule, MatTooltipModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FuseSharedModule } from '@fuse/shared.module';
+import { FuseMaterialColorPickerModule, FuseConfirmDialogModule } from '@fuse/components';
+
 
 const routes = [
   {
@@ -27,11 +32,38 @@ const routes = [
 
 
 @NgModule({
-  declarations: [RegistrationFormComponent, ListComponent, CategoriesComponent],
+  declarations: [RegistrationFormComponent, 
+    ListComponent,
+     CategoriesComponent,
+     ],
   imports: [
     RouterModule.forChild(routes),
+    MatIconModule,
+    BrowserAnimationsModule,
+    
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatRippleModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTooltipModule,
 
-    CommonModule
+    
+
+    FuseSharedModule,
+    FuseConfirmDialogModule,
+    FuseMaterialColorPickerModule,
+    CommonModule,
+  
   ]
 })
 export class GncEventsModule { }
